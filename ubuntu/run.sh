2ae3aae2f25beb40a4b0009f8508f2bb291b09b6
arekbee <961862+arekbee@users.noreply.github.com>
2021-04-09 12:49:54 +0000

    Script update

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-ansible-lint containers.yaml
-ansible-playbook --ask-become-pass containers.yaml
+
+# if [ $# -gt 0 ]; then
+#     echo "Your command line contains $# arguments"
+# else
+#     echo "Your command line contains no arguments"
+#     exit 1
+# fi
+file=polkadot.yaml
+ansible-lint $file
+ansible-playbook --ask-become-pass $file
