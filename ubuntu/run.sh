40e029452518ed688bb1d4d1f42258b10802ab00
arekbee <961862+arekbee@users.noreply.github.com>
2023-07-24 14:06:04 +0200

    New files

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# if [ $# -gt 0 ]; then
-#     echo "Your command line contains $# arguments"
-# else
-#     echo "Your command line contains no arguments"
-#     exit 1
-# fi
-file=containers.yaml
-ansible-lint $file
-ansible-playbook -vvv --ask-become-pass $file
+if [ $# -gt 0 ]; then
+     echo "Your command line contains $# arguments"
+else
+     echo "Your command line contains no arguments"
+     exit 1
+fi
+
+for file ;do
+     printf "Processing file '%s'.\n" "$file"
+     ansible-lint $file
+     ansible-playbook -vvv --ask-become-pass $file
+done
+  
