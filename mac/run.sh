9fe05fdce54afe65eec01676b9c06ac21a6a6f1e
arekbee <961862+arekbee@users.noreply.github.com>
2023-07-31 19:59:06 +0200

    Create run.sh

---
+++ mac/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -gt 0 ]; then
+     echo "Your command line contains $# arguments"
+else
+     echo "Your command line contains no arguments"
+     exit 1
+fi
+
+for file ;do
+     printf "Processing file '%s'.\n" "$file"
+     ansible-lint $file
+     ansible-playbook -vvv --ask-become-pass $file
+done
